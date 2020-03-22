9840877c58e5055a5504ec896ba392593cb4e345
slieman <slieman2001@hotmail.com>
2020-03-22 15:19:01 +0200

    up.

---
+++ resources/js/admin/components/FormElements/EInputNum.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-form-item  :prop="valid">
+    <el-input-number 
+    :style="Inputstyle"
+    :size="size"
+    :show-password="isPassword"
+    :clearable="clearable"
+    :prefix-icon="icon"
+    type="text"
+    v-model="model" 
+    :placeholder="placeholder" 
+    @change="$emit('input',model)"
+    :disabled="isDisabled" />
+  </el-form-item>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["placeholder","clearable","icon","isPassword","isDisabled","valid","size","width"],
+  data() {
+    return {
+      model: ""
+    };
+  },
+  computed:{
+    Inputstyle(){
+     return {
+        width:this.width
+     }
+    }
+  },
+  mounted() {
+    // console.log(this.placeholder)
+  }
+};
+</script>
+
+<style>
+</style>
